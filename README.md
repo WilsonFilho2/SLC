193ff0a13062e9106dedee3052de8014bb4e02e9
WilsonFilho2 <wilson.menezes09@aluno.ifce.edu.br>
2023-04-13 16:48:26 -0300

    Login e Cadastro

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SLC
 Sistema de Listas de Compras
+
+Usuário:
+    -- Nome: aluno
+    -- Senha: aluno123
+
+
+Super Usuário:
+    -- Nome: wilson
+    -- Senha: wilson123
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import request
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.contrib.auth.models import User
 
+
 # Create your views here.
 
 def index(request):
@@ -13,12 +14,30 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        pass
+        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            django_login(request, user=user)
+            return redirect('index')
+        
+        else:
+            return render(request, 'app/login.html', {
+                'message': 'Usuário Não existente',
+            })
+
+
     return render(request, 'app/login.html')
 
 
 def logout(request):
-    pass
+    django_logout(request)
+    return render(request, 'app/login.html', {
+        'message': "Log out",
+    })
 
 
 def cadastro(request):
@@ -27,5 +46,19 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+
+        try: # se fer erro, é porque já existe
+            user = authenticate(username=username, email=email, password=password)
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            user = authenticate(username=username, email=email, password=password)
+            django_login(request, user=user)
+            return redirect('index')
+        
+        except:
+            return render(request, 'app/cadastro.html',{
+                'message': 'Usuário Inválido ou Já existente'
+            })        
+        
 
     return render(request, 'app/cadastro.html')
